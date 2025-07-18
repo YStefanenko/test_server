b27b3dc2841ebf9b8e74ad8e875f549058d036f2
YStefanenko <121567456+YStefanenko@users.noreply.github.com>
2025-07-18 10:39:56 +0100

    Update server.py

--- server.py
+++ server.py
@@ -43,8 +43,9 @@ class GameRoom:
     async def add_player(self, player):
         self.players.append(player)
         if not len(self.players) == 1:
-            await send_pickle(player.writer, self.custom_map)
+            await send_pickle(player.writer, pickle.dumps(self.custom_map))
         if (len(self.players) > 1 and self.mode == '1v1') or (len(self.players) > 3 and self.mode == '2v2'):
+            await asyncio.sleep(5)
             await self.start()
 
     async def start(self):
